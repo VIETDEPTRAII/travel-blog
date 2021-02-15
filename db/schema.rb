43c7f55b108bbf3d2f49555b47b5c8ff2394ffe8
NGUYEN THANH VIET <nguyenthanhviet31031998@gmail.com>
2021-02-15 23:07:26 +0700

    Add validations to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_132416) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
